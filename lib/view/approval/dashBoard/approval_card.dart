5416b6af23054a192985a17fbc49e8f38c02c382
A <solardayz@naver.com>
2021-11-04 11:47:23 +0900

    상세화면 만드는 중 결재대기, 결재중 등.

--- lib/view/approval/dashBoard/approval_card.dart
+++ lib/view/approval/dashBoard/approval_card.dart
@@ -1,11 +1,8 @@
 import 'package:approval/models/recent_file.dart';
-import 'package:approval/view/approval/dashBoard/recent_card.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ApprovalCard extends StatefulWidget {
   final String? title;
-
   const ApprovalCard({Key? key, String? this.title}) : super(key: key);
 
   @override
@@ -15,31 +12,54 @@ class ApprovalCard extends StatefulWidget {
 class _ApprovalCardState extends State<ApprovalCard> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: 30,
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.orange,
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              '결재대기',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ListView.separated(
+              itemCount: demoRecentFiles.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: 10,
+                );
+              },
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      10,
+                    ),
+                    height: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      'saf',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
-        Text(
-          "DASH BOARD",
-          style: Theme.of(context).textTheme.headline4,
-        ),
-        ListView.separated(
-          itemCount: demoRecentFiles.length,
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          separatorBuilder: (context, index) {
-            return SizedBox(
-              height: 10,
-            );
-          },
-          itemBuilder: (context, index) {
-            return RecentCard(demoRecentFiles[index]);
-          },
-        ),
-      ],
+      ),
     );
   }
 }
